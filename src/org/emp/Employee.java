3c66dd94d82aa927486c8ea22b8f74add2084126
muneez89 <muneez89@gmail.com>
2021-12-19 21:41:25 +0530

    Revert "jeeva commit"
    This reverts commit a04c791f0a84f1a30f217f16cef3b46420e9e362.

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -2,8 +2,7 @@ package org.emp;
 
 public class Employee {
 	
-	private void empID() {	
-		System.out.println("jeeva edit ");
+	private void empID() {		
 System.out.println("Employee Details");
 	}
 	
